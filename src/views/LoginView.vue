024eccbfbf350b5811cdf198eed50c5d024f9690
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2022-12-22 16:11:31 +0100

    + Validation #1

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,30 +1,44 @@
 <template>
-  <v-container fill-height fluid class="justify-center">
-    <v-card width="500">
-      <v-card-title>Login</v-card-title>
-      <v-card-text>
-        <v-text-field v-model="username" label="Benutzername" outlined>
-        </v-text-field>
-        <v-text-field
-          v-model="password"
-          label="Passwort"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="showPassword ? 'text' : 'password'"
-          outlined
-          @click:append="showPassword = !showPassword"
-        >
-        </v-text-field>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer> <v-btn large @click="login">Login</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-container>
+  <ValidationObserver>
+    <v-container fill-height fluid class="justify-center">
+      <v-card width="500">
+        <v-card-title>Login</v-card-title>
+        <v-card-text>
+          <ValidationProvider v-slot="{ errors }" rules="required">
+            <v-text-field
+              v-model="username"
+              label="Benutzername"
+              :error-messages="errors"
+              outlined
+            >
+            </v-text-field>
+          </ValidationProvider>
+          <ValidationProvider v-slot="{ errors }" rules="required">
+            <v-text-field
+              v-model="password"
+              label="Passwort"
+              :error-messages="errors"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPassword ? 'text' : 'password'"
+              outlined
+              @click:append="showPassword = !showPassword"
+            >
+            </v-text-field>
+          </ValidationProvider>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer> <v-btn large @click="login">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </ValidationObserver>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { ValidationObserver, ValidationProvider } from "vee-validate";
 export default Vue.extend({
+  components: { ValidationObserver, ValidationProvider },
   data: () => ({
     username: "",
     password: "",
