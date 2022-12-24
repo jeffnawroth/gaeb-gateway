4476540205845e9575088e1c6f53d0a7180fa881
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2022-12-24 09:44:22 +0100

    + Use cors

--- gaeb-gateway-backend/Program.cs
+++ gaeb-gateway-backend/Program.cs
@@ -1,4 +1,17 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy
+                          .AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
 
 // Add services to the container.
 
@@ -17,6 +30,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
