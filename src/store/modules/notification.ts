b32a662160a7868b78310991cf04ae1b9354f5e5
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2022-12-27 18:00:03 +0100

    ^Refactoring #3

--- src/store/modules/notification.ts
+++ src/store/modules/notification.ts
@@ -6,24 +6,23 @@ export default {
   },
 
   mutations: {
-    PUSH(state: any, notifications: any) {
+    PUSH(state: any, notification: any) {
       state.notifications.push({
-        ...notifications,
+        ...notification,
         id: nextId++,
       });
     },
-    DELETE(state: any, notificationToRemove: any) {
-      state.notifications = state.notifications.filter(
-        (notification: any) => notification.id !== notificationToRemove.id
-      );
+    DELETE(state: any, notification: any) {
+      const index = state.notifications.indexOf(notification);
+      state.notifications.splice(index, 1);
     },
   },
   actions: {
     add({ commit }: any, notification: any) {
       commit("PUSH", notification);
     },
-    remove({ commit }: any, notificationToRemove: any) {
-      commit("DELETE", notificationToRemove);
+    remove({ commit }: any, notification: any) {
+      commit("DELETE", notification);
     },
   },
   getters: {},
