a39a58a097372b3cb52dd908abf64ff6e753e0ca
ErenC61 <ec_61@hotmail.de>
2023-01-02 16:27:12 +0100

    + UserRegistrationRequestDto #2

---
+++ gaeb-gateway-backend/Models/DTOs/UserRegistrationRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace gaeb_gateway_backend.Models.DTOs
+{
+    // The expecting body of the request
+	public class UserRegistrationRequestDto
+	{
+        
+        
+		public string firstName { get; set; }
+        
+       
+        //public string lastName { get; set; }
+
+       
+        public string email { get; set; }
+
+        
+        public string password { get; set; }
+	}
+}
+
