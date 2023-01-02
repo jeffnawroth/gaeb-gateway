03b73b761cd3ca75f1544140e4e178895a7d8958
ErenC61 <ec_61@hotmail.de>
2023-01-02 17:36:21 +0100

    + UserLoginRequestDto #1

---
+++ gaeb-gateway-backend/Models/DTOs/UserLoginRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace gaeb_gateway_backend.Models.DTOs;
+
+public class UserLoginRequestDto
+{
+	[Required]
+	public string email { get; set; }
+
+    [Required]
+    public string password { get; set; }
+}
+
