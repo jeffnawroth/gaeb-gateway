df1f49e8ada6cafb4429a7e69d1ebba8c80341a4
ErenC61 <ec_61@hotmail.de>
2023-01-02 16:21:44 +0100

    + Added user model #1

---
+++ gaeb-gateway-backend/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace gaeb_gateway_backend.Models
+{
+	public class User
+	{
+		public int id { get; set; }
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+		public string email { get; set; }
+		public string password { get; set; }
+	}
+}
+
