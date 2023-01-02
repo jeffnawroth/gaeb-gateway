31f209a0e6b99f637eb8f977168c9cf8b5bf5a23
ErenC61 <ec_61@hotmail.de>
2023-01-02 16:30:09 +0100

    + User to DBContext

--- gaeb-gateway-backend/Data/ApiDbContext.cs
+++ gaeb-gateway-backend/Data/ApiDbContext.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using gaeb_gateway_backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace gaeb_gateway_backend.Data;
 
-public class ApiDbContext : DbContext
+public class ApiDbContext : IdentityDbContext
 {
+    public DbSet<User> Users { get; set; }
 
     public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
     {
+
     }
 }
 
