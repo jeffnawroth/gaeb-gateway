93cd9d26d4fee45269001a0d0b948e4c3d91418a
ErenC61 <ec_61@hotmail.de>
2023-01-02 16:20:24 +0100

    # Fixed authentication commit #1

--- gaeb-gateway-backend/Program.cs
+++ gaeb-gateway-backend/Program.cs
@@ -1,5 +1,10 @@
-﻿using gaeb_gateway_backend.Data;
+﻿using System.Text;
+using gaeb_gateway_backend.Configurations;
+using gaeb_gateway_backend.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
@@ -25,7 +30,37 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ApiDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("PostgresqlDbConnection")));
 
-builder.Services.Configure<JwtConfi>
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
+
+// Add Authentication
+builder.Services.AddAuthentication(options =>
+{
+    // Set default authentication scheme
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(jwt =>
+{
+    // Convert Secret String to Array of bytes
+    var key = Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtConfig:Secret").Value);
+
+    jwt.SaveToken = true;
+    // Verify the token
+    // Compare token if it is valid
+    jwt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false, // for development purpose
+        ValidateAudience = false, // for development purpose
+        RequireExpirationTime = false, // for dev -- needs to be updated when refresh token is added
+        ValidateLifetime = true
+    };
+});
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApiDbContext>();
 
 
 var app = builder.Build();
@@ -40,6 +75,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
