4fe8422b91fe77ab95c3a8097797b15e2e3424b2
ErenC61 <ec_61@hotmail.de>
2023-01-04 14:33:01 +0100

    # Fixed login property syntax #5

--- gaeb-gateway-backend/Controllers/AuthenticationController.cs
+++ gaeb-gateway-backend/Controllers/AuthenticationController.cs
@@ -181,7 +181,11 @@ private string GenerateJwtToken(ApplicationUser user)
             new Claim(JwtRegisteredClaimNames.Sub, user.Email),
             new Claim(JwtRegisteredClaimNames.Email, value:user.Email),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-            new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToUniversalTime().ToString())
+            new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToUniversalTime().ToString()),
+            //new
+            new Claim(ClaimTypes.Name, user.FirstName),
+            new Claim(ClaimTypes.Surname, user.LastName),
+            new Claim(ClaimTypes.Email, user.Email)
                 }),
 
             // Set expire time for token
