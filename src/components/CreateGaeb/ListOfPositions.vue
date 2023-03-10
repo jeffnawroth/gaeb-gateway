5bc51e1fddd5f011d6f5ef2dbbefec0743c5e9c7
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2023-03-10 18:22:13 +0100

    ^ Refactoring

---
+++ src/components/CreateGaeb/ListOfPositions.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card>
+    <v-list>
+      <v-list-item-group
+        v-model="selectedItems"
+        multiple
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.id"
+          @click="$emit('highlight-model-element', item.id)"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.name }}
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ item.description }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data: () => ({
+    selectedItems: [] as any,
+    items: [
+      {
+        id: 6546,
+        name: "Door",
+        description: "Lorem Ipsum",
+      },
+      {
+        id: 481,
+        name: "Wall",
+        description: "Lorem Ipsum",
+      },
+      {
+        id: 7290,
+        name: "Window",
+        description: "Lorem Ipsum",
+      },
+      {
+        id: 7482,
+        name: "Roof",
+        description: "Lorem Ipsum",
+      },
+    ] as any,
+  }),
+  methods: {
+    toggleListElement(modelId: number) {
+      const listItemIndex = this.items.findIndex(
+        (item: any) => item.id == modelId
+      );
+      if (listItemIndex === -1) return;
+
+      this.selectedItems.includes(listItemIndex)
+        ? this.selectedItems.splice(
+            this.selectedItems.indexOf(listItemIndex),
+            1
+          )
+        : this.selectedItems.push(listItemIndex);
+    },
+  },
+});
+</script>
--- src/views/CreateGaeb.vue
+++ src/views/CreateGaeb.vue
@@ -4,36 +4,14 @@
       <ModelViewer
         ref="modelViewer"
         @toggle-list-element="toggleListElement"
-        @reset-selected-items="selectedItems = []"
+        @reset-selected-items="resetSelectedItems"
       />
-      <!-- <v-btn @click="clearSelection">
-        Reset
-      </v-btn> -->
     </v-col>
     <v-col class="d-flex flex-column">
-      <v-card>
-        <v-list>
-          <v-list-item-group
-            v-model="selectedItems"
-            multiple
-          >
-            <v-list-item
-              v-for="item in items"
-              :key="item.id"
-              @click="highlightModelElement(item.id)"
-            >
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ item.name }}
-                </v-list-item-title>
-                <v-list-item-subtitle>
-                  {{ item.description }}
-                </v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-card>
+      <ListOfPositions
+        ref="listOfPositions"
+        @highlight-model-element="highlightModelElement"
+      />
       <v-spacer />
       <div class="d-flex justify-end">
         <v-btn width="350">
@@ -47,56 +25,26 @@
 <script lang="ts">
 import Vue from "vue";
 
-import { mapActions } from "vuex";
 import ModelViewer from "@/components/CreateGaeb/ModelViewer.vue";
+import ListOfPositions from "@/components/CreateGaeb/ListOfPositions.vue";
 export default Vue.extend({
-  components: { ModelViewer },
-  data: () => ({
-    selectedItems: [] as any,
-    items: [
-      {
-        id: 6546,
-        name: "Door",
-        description: "Lorem Ipsum",
-      },
-      {
-        id: 481,
-        name: "Wall",
-        description: "Lorem Ipsum",
-      },
-      {
-        id: 7290,
-        name: "Window",
-        description: "Lorem Ipsum",
-      },
-      {
-        id: 7482,
-        name: "Roof",
-        description: "Lorem Ipsum",
-      },
-    ] as any,
-  }),
-
+  components: { ModelViewer, ListOfPositions },
+  data: () => ({}),
   methods: {
-    ...mapActions("notification", ["add"]),
-
-    toggleListElement(modelId: number) {
-      const listItemIndex = this.items.findIndex(
-        (item: any) => item.id == modelId
-      );
-      if (listItemIndex === -1) return;
-
-      this.selectedItems.includes(listItemIndex)
-        ? this.selectedItems.splice(
-            this.selectedItems.indexOf(listItemIndex),
-            1
-          )
-        : this.selectedItems.push(listItemIndex);
-    },
     highlightModelElement(id: number) {
       (
         this.$refs.modelViewer as InstanceType<typeof ModelViewer>
       ).highlightModelElement(id);
+    },
+    toggleListElement(id: number) {
+      (
+        this.$refs.listOfPositions as InstanceType<typeof ListOfPositions>
+      ).toggleListElement(id);
+    },
+    resetSelectedItems() {
+      (
+        this.$refs.listOfPositions as InstanceType<typeof ListOfPositions>
+      ).selectedItems = [];
     },
   },
 });
