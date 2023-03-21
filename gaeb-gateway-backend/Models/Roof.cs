a439d96732a0d40c86e95e1c9efb9e635407da9b
ErenC61 <ec_61@hotmail.de>
2023-03-21 19:49:07 +0100

    + Added functionality to retrieve more elements #18

---
+++ gaeb-gateway-backend/Models/Roof.cs
@@ -0,0 +1,10 @@
+namespace gaeb_gateway_backend.Models;
+
+public class Roof: BuildingElement
+{
+    public string area;
+    public string thickness;
+    public string volume;
+    public string roughness;
+    public string thermalResistance;
+}
