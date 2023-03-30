9e5ee27cd6c96c170cf9659e9e1a0ac659a7b654
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2023-03-30 09:34:40 +0200

    ^ Filter

--- src/store/modules/projects.ts
+++ src/store/modules/projects.ts
@@ -90,7 +90,13 @@ export default {
   },
   getters: {
     getFilteredProjects: (state: ProjectState) => {
-      return state.projects;
+      //return state.projects;
+      return state.projects.filter((project) => {
+        return (
+          project.id.endsWith("-281878346acf") &&
+          !project.name.toLocaleLowerCase().includes("test")
+        );
+      });
     },
   },
 };
